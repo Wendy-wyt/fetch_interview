2c70d0aab74b5c61a682621f2c15f76657642302
Wendy-wyt <wuyuting0101@gmail.com>
2024-06-22 13:16:54 -0400

    init data types

---
+++ fetch_interview/Items/Item.swift
@@ -0,0 +1,52 @@
+import Foundation
+import SwiftData
+
+@Model
+final class Category {
+    var name: String = ""
+    var meals: [CategoryMeal] = []
+    
+    init(name: String, meals: [CategoryMeal]) {
+        self.name = name
+        self.meals = meals
+    }
+}
+
+@Model
+final class CategoryMeal {
+    var strMeal: String
+    var strMealThumb: String
+    var idMeal: String
+    
+    init(strMeal: String, strMealThumb: String, idMeal: String) {
+        self.strMeal = strMeal
+        self.strMealThumb = strMealThumb
+        self.idMeal = idMeal
+    }
+}
+
+@Model
+final class Meal {
+    var strMeal: String = ""
+    var idMeal: String = ""
+    var strInstructions: String = ""
+    var ingredients: [Ingredient]
+    
+    init(strMeal: String, idMeal: String, strInstructions: String, ingredients: [Ingredient]) {
+        self.idMeal = idMeal
+        self.ingredients = ingredients
+        self.strMeal = strMeal
+        self.strInstructions = strInstructions
+    }
+}
+
+@Model
+final class Ingredient {
+    var strIngredient: String
+    var strMeasure: String
+    
+    init(strIngredient: String, strMeasure: String) {
+        self.strIngredient = strIngredient
+        self.strMeasure = strMeasure
+    }
+}
