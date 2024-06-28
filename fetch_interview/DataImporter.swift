4b28b41366eebf5a1be5b8e94bc474aa28c3204e
Yuting Wu <wuyuting0101@gmail.com>
2024-06-28 00:12:17 -0400

    Built basic meal details function

---
+++ fetch_interview/DataImporter.swift
@@ -0,0 +1,8 @@
+//
+//  DataImporter.swift
+//  fetch_interview
+//
+//  Created by Yuting Wu on 6/27/24.
+//
+
+import Foundation
---
+++ fetch_interview/URLImage.swift
@@ -0,0 +1,18 @@
+//
+//  URLImage.swift
+//  fetch_interview
+//
+//  Created by Yuting Wu on 6/27/24.
+//
+
+import SwiftUI
+
+struct URLImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    URLImage()
+}
