05c7f01d5b565eff09c4bd9b769aae51b00ac909
Yuting Wu <wuyuting0101@gmail.com>
2024-06-28 13:06:51 -0400

    Finish product

---
+++ fetch_interview/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  fetch_interview
+//
+//  Created by Yuting Wu on 6/28/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
---
+++ fetch_interview/Views/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  fetch_interview
+//
+//  Created by Michael Zhang on 6/22/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+
+    @Query private var meals: [MealShortModel]
+    
+    var body: some View {
+        NavigationView {
+            VStack{
+                List {
+                    Text("Desert").bold()
+                    ForEach(meals) { meal in
+                        NavigationLink(destination: DetailView(meal: meal)) {
+                            HStack {
+                                URLImage(urlString: meal.strMealThumb, width: 130, height: 70)
+                                Text(meal.strMeal)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
